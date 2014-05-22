b084504e6a7aa887fdb1ba562337578f78647f53
Anthony Vargo <anthony.vargo@doublelinepartners.com>
2014-05-22 14:44:28 -0500

    Added description and note regarding Microsoft.WebApplication.targets.

--- CleanAll.ps1
+++ CleanAll.ps1
@@ -1,6 +1,17 @@
+# *************************************************************************
+# CleanAll
+#
+# 	Recursively builds the Clean target in the current directory and all
+# 	subdirectories that contain a .csproj file. Note that MSBuild must be
+# 	included in your PATH environment variable. For VS2013, you will need
+#	to ensure the proper MSBuild is included in the PATH in order to
+#	clean any projects referencing Microsoft.WebApplication.targets:
+#
+#		C:\Program Files\MSBuild\12.0\Bin for 32-bit machines,
+#		C:\Program Files (x86)\MSBuild\12.0\Bin for 64-bit machines
+#
+# *************************************************************************
 Get-ChildItem -Path .\ -Recurse -Filter *.csproj | `
 Foreach-Object {
 	msbuild $_.FullName /t:Clean
 }
-
-#C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe [path]\[project].csproj /t:Clean
